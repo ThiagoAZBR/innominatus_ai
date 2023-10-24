57e3b32ad21363de70350ff4d45b8cdcd0a67eba
Thiago A Zeferino <thiagoaugustozeferino@gmail.com>
2023-10-24 07:49:57 -0300

    🐛 fix: problem with api and update prompt

--- lib/app/shared/app_constants/app_constants.dart
+++ lib/app/shared/app_constants/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
       'Olá!\n\nEstou aqui para responder suas dúvidas e perguntas sobre os conteúdos!\n\nMe envie suas dúvidas e ficarei feliz em poder ajudar.';
   // Prompts
   static String getRoadmap(String content) =>
-      'Crie um Roadmap de estudos sobre $content e escreva em formato de Json, onde será um array de objetos, onde cada objeto terá a apenas a chave "content"';
+      'Você é uma IA especializada em educação. Crie um Roadmap de estudos sobre $content e escreva em formato de Json, onde será um array de objetos, onde cada objeto terá a apenas a chave "content"';
   static const String getFieldsOfStudy =
       'Escreva em formato de Json, onde terá uma chave "items" e um valor que vai ser um Array de Strings. Dentro do Array, escreve o máximo possível áreas de estudo escolhidas na faculdade e em alta no mercado de trabalho';
   static String createClass(String className) =>
--- lib/app/shared/network/app_urls.dart
+++ lib/app/shared/network/app_urls.dart
@@ -1,6 +1,6 @@
 class AppUrls {
   static const createChatCompletionProduction =
-      'https://aicsforwba.execute-api.us-east-1.amazonaws.com/chat';
+      'https://express-api-chaos-production.up.railway.app/chat';
   static const streamCreateChatCompletionProduction =
       'https://express-api-chaos-production.up.railway.app/stream?content=';
 }
